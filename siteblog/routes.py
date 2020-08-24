cb23743f8304d9b0bb3c4b60de753707b29b69e5
taroken6 <audiemendaros@yahoo.com>
2020-08-24 14:16:22 -0700

    Projecture Structure blueprint

--- siteblog/routes.py
+++ siteblog/routes.py
@@ -6,3 +6,33 @@ from siteblog import app
 @app.route('/home')
 def home():
     return '<h1>Hello world!<h1>'
+
+
+@app.route('/projects')
+def projects():
+    return ''
+
+
+@app.route('/blog')
+def blog():
+    return ''
+
+
+@app.route('/register')
+def register():
+    return ''
+
+
+@app.route('/login')
+def login():
+    return ''
+
+
+@app.route('/logout')
+def logout():
+    return ''
+
+
+@app.route('account')
+def account():
+    return ''
